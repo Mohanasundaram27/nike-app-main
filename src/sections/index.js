0309642634e94a418f36d8ac53cfce57e4baa076
Mohanasundaram27 <107344273+Mohanasundaram27@users.noreply.github.com>
2024-04-06 19:22:52 +0530

    Add files via upload

---
+++ src/sections/index.js
@@ -0,0 +1,21 @@
+import CustomerReviews from "./CustomerReviews";
+import Footer from "./Footer";
+import Hero from "./Hero";
+import PopularProduct from "./PopularProduct";
+import Services from "./Services";
+import SpecialOffer from "./SpecialOffer";
+import Subscribe from "./Subscribe";
+import SuperQuality from "./SuperQuality";
+import Nav from "../components/Nav";
+
+export {
+  CustomerReviews,
+  Footer,
+  Hero,
+  PopularProduct,
+  Services,
+  SpecialOffer,
+  Subscribe,
+  SuperQuality,
+  Nav,
+};
